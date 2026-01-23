f381bfb51cf00c7b94a2998d91b00f8bbfc89e04
Kauã Silva <kakaandradesilva2008@hotmail.com>
2026-01-22 21:56:32 -0300

    feat: create a function to add products to the cart.

--- src/app/(store)/(home)/product/[slug]/page.tsx
+++ src/app/(store)/(home)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AddToCartButton } from "@/components/add-to-card-button";
 import { api } from "@/data/api";
 import { Product } from "@/data/types/products";
 import { Metadata } from "next";
@@ -6,6 +7,7 @@ import Image from "next/image";
 interface ProductProps {
   params: Promise<{
     slug: string;
+    id: number;
   }>;
 }
 
@@ -23,8 +25,6 @@ async function getProduct(slug: string): Promise<Product> {
   }
 
   const product = await response.json();
-
-  console.log("API RESPONSE:", product);
 
   return product;
 }
@@ -83,6 +83,7 @@ export default async function ProductPage({ params }: ProductProps) {
             })}
           </span>
           <span className="text-sm text-zinc-400">
+            em até 12x s/ de{" "}
             {(product.price / 12).toLocaleString("pt-BR", {
               style: "currency",
               currency: "BRL",
@@ -121,12 +122,7 @@ export default async function ProductPage({ params }: ProductProps) {
           </div>
         </div>
 
-        <button
-          type="button"
-          className="mt-8 flex h-12 items-center justify-center rounded-full bg-emerald-600 font-semibold text-white"
-        >
-          Adicionar ao carrinho
-        </button>
+        <AddToCartButton productId={product.id} />
       </div>
     </div>
   );
